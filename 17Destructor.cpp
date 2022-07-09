ac429ac68071149195ecef6122e17ce5e25e2d57
Harshita Sharma <harshitasharma18.11.2000@gmail.com>
2022-07-09 09:56:22 +0530

    Create 17Destructor.cpp

---
+++ 17Destructor.cpp
@@ -0,0 +1,53 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+class student{
+     string name;
+  public:
+  int age;
+  bool gender;
+  
+  ~student(){
+      cout<<"Destructor called"<<endl;//at end of main fucntion
+  }
+  
+  void setName(string s){
+      name=s;
+  }
+  void getName(){
+      cout<<name<<endl;
+  }
+  student(){
+      cout<<"Default Constructor"<<endl;
+  }
+  student(string s,int a,bool k){
+      cout<<"Parameterised Constructor"<<endl;
+      name=s;
+      age=a;
+      gender=k;
+  }
+  student(student &s){
+       cout<<"Copy Constructor"<<endl;
+      name=s.name;
+      age=s.age;
+      gender=s.gender;
+  }
+    void printInfo(){
+        cout<<"Name=";
+        cout<<name<<endl;
+         cout<<"Age=";
+        cout<<age<<endl;
+         cout<<"Gender=";
+        cout<<gender<<endl;
+    }
+    
+};
+int main() {
+ 
+student a("Tim",18,1);
+student b;
+//or student c(a);
+student c=a;//by default shallow copy else here deep copy as copy constructor present
+
+    return 0;
+}
