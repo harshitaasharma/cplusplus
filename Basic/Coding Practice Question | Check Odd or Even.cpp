ea77bbcffff6f0ed0052859ebb7be66b85c9ac75
Harshita Sharma <harshitasharma18.11.2000@gmail.com>
2022-09-10 11:21:24 +0530

    Create Coding Practice Question | Check Odd or Even.cpp

---
+++ Basic/Coding Practice Question | Check Odd or Even.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //find even or odd number
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"even number";
+    }
+    else{
+        cout<<"odd number";
+    }
+ return 0;   
+}
