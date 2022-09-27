769a641a66ca7fd729ca090e2f0a95e74818f2de
Harshita Sharma <harshitasharma18.11.2000@gmail.com>
2022-09-27 15:50:21 +0530

    Create 2traverse.cpp

---
+++ Linked List/2traverse.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+  public:
+  int data;
+  Node* next;
+  
+  Node(int data){
+      this->data=data;
+      this->next=NULL;
+  }
+  
+};
+
+void print(Node* &head){
+    cout<<endl;
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    
+    Node* node1=new Node(10);
+    // cout<<"Data="<<node1->data<<endl;
+    // cout<<"Next="<<node1->next<<endl;
+    
+    Node* head=node1;
+    
+    print(head);
+    
+    
+    return 0;
+}
