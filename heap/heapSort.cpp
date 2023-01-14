67653cb7652bcc74c0359405c56bb5e3c08343f5
Harshita Sharma <harshitasharma18.11.2000@gmail.com>
2023-01-14 15:40:56 +0530

    Create heapSort.cpp

---
+++ heap/heapSort.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class heap{
+public:
+int arr[100];
+int size=0;
+
+void insert(int n){
+    size++;
+    int index=size;
+    arr[index]=n;
+
+    while(index>1){
+        int parent=index/2;
+        if(arr[index]>arr[parent]){
+            swap(arr[index],arr[parent]);
+            index=parent;
+        }
+        else{
+            return;
+        }
+    }
+
+}
+
+void deleted(){
+    if(size==0){
+        return;
+    }
+arr[1]=arr[size];
+size--;
+int i=1;
+while(i<size){
+    int left=2*i;
+    int right=2*i+1;
+
+    if(left<size && arr[i] <arr[left]){
+        swap(arr[i],arr[left]);
+        i=left;
+    }
+    else if(right<size && arr[i] <arr[right]){
+        swap(arr[i],arr[right]);
+        i=right;
+    }
+    else{
+        return;
+    }
+
+}
+}
+
+
+void print(){
+    for(int i=1;i<=size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+};
+
+
+void heapify(int arr[],int n,int i){
+int largest=i;
+int left=2*i;
+int right=(2*i)+1;
+
+if(left<=n && arr[left]> arr[largest]){
+    largest=left;
+}
+if(right<=n && arr[right]> arr[largest]){
+    largest=right;
+}
+if(largest!=i){
+    swap(arr[largest],arr[i]);
+    heapify(arr,n,largest);
+}
+
+
+
+}
+
+void heapSort(int arr[],int n){
+    int size=n;
+    while(size>1){
+        swap(arr[size],arr[1]);
+        size--;
+        heapify(arr,size,1);//on root node
+
+    }
+}
+
+int main(){
+
+heap h;
+h.insert(50);
+h.insert(55);
+h.insert(53);
+h.insert(52);
+h.insert(54);
+h.print();
+cout<<endl;
+h.deleted();
+h.print();
+
+cout<<endl;
+int arr[6]={-1,54,53,55,52,50};
+for(int i=5/2;i>0;i--){
+heapify(arr,5,i);
+}
+cout<<"printing array"<<endl;
+for(int i=1;i<=5;i++){
+    cout<<arr[i]<<" ";
+}
+heapSort(arr,5);
+cout<<"printing array"<<endl;
+for(int i=1;i<=5;i++){
+    cout<<arr[i]<<" ";}
+    return 0;
+}
